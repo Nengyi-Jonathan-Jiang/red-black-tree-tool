8e6b125b3baf7154200c4a0a5e81bf7200025ea8
slhscs212 <theNerdyMango@gmail.com>
2023-01-26 08:01:11 -0600

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    $('.red-black-tree.node,.red-black-tree.nil').connections('remove');
+    document.querySelectorAll('.red-black-tree.node').forEach(i => {
+        
+    })
+}, 100);
