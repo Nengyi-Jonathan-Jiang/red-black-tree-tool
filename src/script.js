9b43f709fa6d0d18e0ac09dc55e208087f3fa8f5
slhscs212 <theNerdyMango@gmail.com>
2023-01-26 08:05:15 -0600

    .

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 setInterval(() => {
-    $('.red-black-tree.node,.red-black-tree.nil').connections('remove');
-    document.querySelectorAll('.red-black-tree.node').forEach(i => {
-        
+    $('.red-black-tree .node,.red-black-tree .nil').connections('remove');
+    document.querySelectorAll('.red-black-tree.node').forEach(p => {
+        console.log(p);
+
+        p.id = "TEMP_UPDATE_CONNECTIONS";
+        $().connections({ from: '#TEMP_UPDATE_CONNECTIONS', to: '.red-black-tree .node, .red-black-tree .nil' });
+        p.id = "";
     })
-}, 100);
+}, 1000);
+
+// INSERT 37 42 47 21 46 18 30 33 9 6 7 45 3 29 43
