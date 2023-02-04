f10425fb875518b8e8bedbe02d4f8968f6f5f62f
Jonathan Jiang <theNerdyMango@gmail.com>
2023-02-04 17:58:18 -0600

    .

--- src/script.js
+++ src/script.js
@@ -1,15 +1,5 @@
-// INSERT 37 42 47 21 46 18 30 33 9 6 7 45 3 29 43
-
 class N {
-    public el: HTMLElement;
-    public input: HTMLSpanElement;
-    private _value: string;
-    private _color: boolean;
-    private lc: N;
-    private rc: N;
-    private _parent: N;
-
-    constructor(value: string) {
+    constructor(value) {
         this.el = document.createElement("div");
         this.input = document.createElement("span");
         this.input.className = "value-input";
@@ -20,7 +10,7 @@ class N {
             this.color = value == null || !this.color;
             e.preventDefault();
             saveStep(this.root);
-        }
+        };
         this.input.onkeydown = e => {
             if (e.key == "Enter") {
                 let s = this.input.innerText.trim();
@@ -30,15 +20,12 @@ class N {
                 }
                 this.input.blur();
                 e.preventDefault();
-
                 N.updateLayout(this.root);
             }
-            // Prevent too long text, prevent non-word characters
             else if (e.key.length == 1 && !e.ctrlKey && !(this.input.innerText.length < 9 && e.key.match(/^[a-zA-Z0-9.\- ]$/)))
                 e.preventDefault();
-
             N.updateLayout(this.root);
-        }
+        };
         this.input.addEventListener('focusout', _ => {
             let s = this.input.innerText.trim();
             if (s != this.value) {
@@ -46,89 +33,71 @@ class N {
                 saveStep(this.root);
             }
             this.input.blur();
-
             N.updateLayout(this.root);
-        })
-
+        });
         this.value = value;
         this.color = false;
-
         N.updateLayout(this.root);
     }
-
     set color(c) {
         this._color = c && !!this._value;
         this.el.className =
             this._value == null ? "nil" :
                 c ? "black node" : "red node";
     }
-
     get color() {
-        return this._color
+        return this._color;
     }
-
-    set value(v: string) {
+    set value(v) {
         if (v == null || v == "" || v == "NIL") {
             this._value = null;
             this.input.innerText = "NIL";
             this.el.className = "nil";
-
             this.lc && this.lc.delete();
             this.rc && this.rc.delete();
             this.lc = this.rc = null;
-        } else {
+        }
+        else {
             this._value = v;
             this.input.innerText = v;
-
             this.lc || (this.left = new N(""));
             this.rc || (this.right = new N(""));
         }
         this.color = this.color;
-
         N.updateLayout(this.root);
     }
-
     get value() {
-        return this._value
+        return this._value;
     }
-
     set left(n) {
         this.lc && this.lc.delete();
         this.lc = n;
-        n && (this.el.appendChild(n.el), n._parent = this)
+        n && (this.el.appendChild(n.el), n._parent = this);
     }
-
     set right(n) {
         this.rc && this.rc.delete();
         this.rc = n;
-        n && (this.el.appendChild(n.el), n._parent = this)
+        n && (this.el.appendChild(n.el), n._parent = this);
     }
-
     get left() {
-        return this.lc
+        return this.lc;
     }
-
     get right() {
-        return this.rc
+        return this.rc;
     }
-
     delete() {
         this.el.parentElement.removeChild(this.el);
         N.updateLayout(this.root);
     }
-
     get parent() {
         return this._parent;
     }
-
-    get root(): N {
+    get root() {
         return this.parent == null ? this : this.parent.root;
     }
-
-    get reverseLevelOrderTraversal() : N[] {
-        const S : N[] = [];
-        const Q : N[] = [];
-
+    get reverseLevelOrderTraversal() {
+        const S = [];
+        const Q = [];
         Q.push(this);
         while (Q.length) {
             const node = Q.shift();
@@ -136,115 +105,94 @@ class N {
             node.left && Q.push(node.left);
             node.right && Q.push(node.right);
         }
-
         return S;
     }
-    public static updateLayout(root: N) {
-        // Re-space nodes
-        for(const node of root.reverseLevelOrderTraversal){
+    static updateLayout(root) {
+        document.body.style.display = 'none';
+        document.body.style.display = 'flex';
+        for (const node of root.reverseLevelOrderTraversal) {
             let w = node.el.getBoundingClientRect().width;
             let iw = node.input.getBoundingClientRect().width;
-
             node.el.dataset.w = w.toString();
-
             let left;
-            if(node.value == null) // NIL node
+            if (node.value == null)
                 left = w / 2;
-            else    // Non-nil
+            else
                 left = (+node.left.el.dataset.l + +node.left.el.dataset.w + +node.right.el.dataset.l) / 2;
-
             node.el.dataset.l = left.toString();
             node.input.style.setProperty("--left", (left - iw / 2).toString());
         }
-
-        // Draw lines
-        [root.left, root.right].filter(i => !!i).map(function process(x: N) {
+        [root.left, root.right].filter(i => !!i).map(function process(x) {
             const [xe, pe] = [x.input, x.parent.input];
             const [pr, xr] = [xe, pe].map(i => i.getBoundingClientRect());
             const [pc, xc] = [pr, xr].map(i => [i.x + i.width / 2, i.y + i.height / 2]);
-
             const [dx, dy] = [xc[0] - pc[0], xc[1] - pc[1]];
             x.el.style.setProperty("--l", Math.sqrt(dx * dx + dy * dy) + "px");
             x.el.style.setProperty("--theta", Math.atan2(dy, dx) + "rad");
-
             x.left && process(x.left);
             x.right && process(x.right);
-        })
+        });
     }
-
-
-    public static parseData(s: string) {
+    static parseData(s) {
         let i = 0;
         let ss = s.replaceAll("%20", " ");
         return this._parseData(() => i >= ss.length ? null : ss[i++]);
     }
-
-    private static _parseData(next: () => string): N {    // Input: s := - | {s:<text>:s}
+    static _parseData(next) {
         let c1 = next();
         if (c1 == '{') {
             const color = next() == 'b';
-            next(); //Advance past ":"
-            const left = this._parseData(next);
-            next(); //Advance past ":"
-            let val = "", c;
-            while ((c = next()) && c != ":") val += c;
-            // We stop after advancing past ":", just parse again
-            const right = this._parseData(next);
-            // Advance past "}"
             next();
-
+            const left = this._parseData(next);
+            next();
+            let val = "", c;
+            while ((c = next()) && c != ":")
+                val += c;
+            const right = this._parseData(next);
+            next();
             const res = new N(val);
             res.color = color;
             res.left = left;
             res.right = right;
             return res;
-        } else if (c1 == '-') {
+        }
+        else if (c1 == '-') {
             return new N("");
         }
-
         return null;
     }
-
-    public static toData(n: N): string {
-        return n == null || n.value == null ? '-' : `{${'rb'[+n.color]}:${this.toData(n.left)}:${n.value}:${this.toData(n.right)}}`
+    static toData(n) {
+        return n == null || n.value == null ? '-' : `{${'rb'[+n.color]}:${this.toData(n.left)}:${n.value}:${this.toData(n.right)}}`;
     }
 }
-
-
-let r: N;
+let r;
 let currStep = -1;
-const steps: string[] = [];
-let lastStep: string = null;
-
-function saveStep(n: N) {
+const steps = [];
+let lastStep = null;
+function saveStep(n) {
     let d = N.toData(n);
-    if (d == lastStep) return;
+    if (d == lastStep)
+        return;
     steps.splice(currStep + 1);
     steps.push(N.toData(n));
     currStep++;
     updateAfterStep(false);
 }
-
 function undo() {
-    canUndo() && currStep--, updateAfterStep()
+    canUndo() && currStep--, updateAfterStep();
 }
-
 function redo() {
-    canRedo() && currStep++, updateAfterStep()
+    canRedo() && currStep++, updateAfterStep();
 }
-
 function canUndo() {
-    return currStep > 0
+    return currStep > 0;
 }
-
 function canRedo() {
-    return currStep + 1 < steps.length
+    return currStep + 1 < steps.length;
 }
-
 function updateAfterStep(updateTree = true) {
     let s = steps[currStep] || '-';
     lastStep = window.location.hash = s;
-
     if (updateTree) {
         let t = N.parseData(s);
         r.value = t.value;
@@ -252,20 +200,15 @@ function updateAfterStep(updateTree = true) {
         r.left = t.left;
         r.right = t.right;
     }
-
     N.updateLayout(r);
     document.getElementById('undo').ariaDisabled = "" + !canUndo();
     document.getElementById('redo').ariaDisabled = "" + !canRedo();
 }
-
 {
     r = window.location.hash && N.parseData(window.location.hash.substring(1)) || new N(null);
-
     document.querySelector('.red-black-tree').appendChild(r.el);
     r.value = r.value;
-
-    // @ts-ignore
     window["r"] = r;
-
     saveStep(r);
 }
+//# sourceMappingURL=script.js.map
